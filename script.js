fb233e4c80394337b21a4d7fcada492884a13e1e
ninerevius <56074364+ninerevius@users.noreply.github.com>
2021-01-04 15:07:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,22 @@
 let btnnacht = document.querySelector('#nacht');
 let btndag = document.querySelector('#dag');
 let btnavond = document.querySelector('#avond');
+let btnvakantie = document.querySelector('#Vakantie');
 
-btnnacht.addEventListener('click', () =>{
+btnnacht.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('IMG/Nacht-100.jpg')"
 });
 
-btndag.addEventListener('click', () =>{
+btndag.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('IMG/Dag-100.jpg')"
 });
 
-btnavond.addEventListener ('click', () =>{
+btnavond.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('IMG/avond-100.jpg')"
+});
+
+btnvakantie.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('IMG/Vakantie_1-100.jpg')"
 });
 
 var DeWortel = document.querySelector('#OranjeWortel');
@@ -23,47 +28,61 @@ var Violin = document.querySelector('#Viool');
 var Mp3Viool = document.querySelector('#VioolMuziek');
 var Schelden = document.querySelector('#Vloeken');
 var Mp3RotHond = document.querySelector('#RotHond');
+var Mp3vakantie = document.querySelector('#Happytunes');
 
-function vies( ) {
+function vies() {
     IGNeutraal.src = 'IMG/IGhatewortel.png';
 }
 
-function normaal () {
+function normaal() {
     IGNeutraal.src = 'IMG/IGblank.png';
 }
 
-function love () {
+function love() {
     IGNeutraal.src = 'IMG/IGlovevis.png';
 }
 
-function blij () {
+function blij() {
     IGNeutraal.src = 'IMG/hover.png';
 }
 
-function Vioolspelen(  ) {
+function blink() {
+    IGNeutraal.src = 'IMG/eyesclosed.png';
+}
+
+function Vioolspelen() {
     Mp3Viool.play();
     Mp3Viool.volume = 0.6;
     IGNeutraal.src = 'IMG/sad.png';
 }
 
-function boos () {
+function boos() {
     IGNeutraal.src = 'IMG/mad.png';
     Mp3RotHond.play();
     Mp3RotHond.volume = 0.6;
 }
 
-function sleep () {
+function sleep() {
     IGNeutraal.src = 'IMG/zzz.png';
+}
+
+function chill() {
+    IGNeutraal.src = 'IMG/ChilleIgloo.png';
+    Mp3vakantie.play();
+    Mp3vakantie.volume = 0.4;
 }
 
 // Bron: https://www.youtube.com/watch?v=1xEfqgElMsY hiermee heb ik de mouseenter & mouseleave mee verwerkt.
 // https://www.youtube.com/watch?v=s9wW2PpJsmQ de link voor het proberen om hem te laten in een Loop () te laten gaan.
 
-DeWortel.addEventListener('dragend', vies) 
-IGNeutraal.addEventListener('click', normaal) 
+DeWortel.addEventListener('dragend', vies)
+IGNeutraal.addEventListener('click', normaal)
 DeVis.addEventListener('dragend', love)
-IGNeutraal.addEventListener('mouseenter', blij)
+IGNeutraal.addEventListener('mouseenter', blink)
 IGNeutraal.addEventListener('mouseleave', normaal)
-Violin.addEventListener ('click', Vioolspelen)
+Violin.addEventListener('click', Vioolspelen)
 Schelden.addEventListener('click', boos)
 btnnacht.addEventListener('click', sleep)
+btnvakantie.addEventListener('click', chill)
+IGNeutraal.addEventListener('dblclick', blij)
+IGNeutraal.addEventListener('click', blij)
